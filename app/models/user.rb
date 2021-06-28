d6549b623fbb846bf3b9c62d4287957fc7107613
hidapen <hidapen0630@gmail.com>
2021-06-28 10:57:29 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :human_first_name,      presence: true
+  validates :human_last_name,       presence: true
+  validates :human_first_name_rubi, presence: true
+  validates :human_last_name_rubi,  presence: true
+  validates :pets_name,             presence: true
+  validates :pets_name_rubi,        presence: true
+  validates :pets_type,             presence: true
+  validates :pets_age,              presence: true
+
 end
