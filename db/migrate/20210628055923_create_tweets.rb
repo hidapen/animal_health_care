78a5bf538c3fb768acf61d8ffa4b6c2254e769eb
hidapen <hidapen0630@gmail.com>
2021-06-28 15:11:04 +0900

    DB生成

---
+++ db/migrate/20210628055923_create_tweets.rb
@@ -0,0 +1,15 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.integer :energy_id,      null: false
+      t.integer :food_id,        null: false
+      t.integer :toilet_id,      null: false
+      t.integer :poop_status_id, null: false
+      t.integer :exercise_id,    null: false
+      t.integer :sleep_id,       null: false
+      t.integer :different_id,   null: false
+      t.references :user,        null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_222431) do
+ActiveRecord::Schema.define(version: 2021_06_28_055923) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "energy_id", null: false
+    t.integer "food_id", null: false
+    t.integer "toilet_id", null: false
+    t.integer "poop_status_id", null: false
+    t.integer "exercise_id", null: false
+    t.integer "sleep_id", null: false
+    t.integer "different_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +48,5 @@ ActiveRecord::Schema.define(version: 2021_06_27_222431) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
