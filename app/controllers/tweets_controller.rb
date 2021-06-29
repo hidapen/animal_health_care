c3fcd345baae882cd0f4caed250756230e2a4e9c
hidapen <hidapen0630@gmail.com>
2021-06-29 16:22:13 +0900

    投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,22 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+  def new
+    @tweet = Tweet.new
+  end
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:energy_id,:food_id,:toilet_id,:poop_status_id,:exercise_id,:sleep_id,:different_id).merge(user_id: current_user.id)
+  end
+
 end
