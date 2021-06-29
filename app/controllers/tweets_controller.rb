8419d3acc26e71c202200923d018fc41ed350966
hidapen <hidapen0630@gmail.com>
2021-06-29 18:07:23 +0900

    詳細/編集/削除機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,25 @@ class TweetsController < ApplicationController
       render :new
     end
   end
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to root_path
+  end
 
 
   private
