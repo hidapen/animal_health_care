66e551be0489b1a4fb0db81986622db7743b17bf
hidapen <hidapen0630@gmail.com>
2021-07-07 00:19:29 +0900

    before_actionの設定とnew/createの変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,17 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
     @tweets = current_user.tweets.all
   end
 
   def new
-    @tweet = current_user.tweets.new
+    @tweet = Tweet.new
   end
 
   def create
-    @tweet = current_user.tweets.new(tweet_params)
+    @tweet = Tweet.new(tweet_params)
     if @tweet.save
       redirect_to root_path
     else
@@ -19,15 +20,12 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = current_user.tweets.find(params[:id])
   end
 
   def edit
-    @tweet = current_user.tweets.find(params[:id])
   end
 
   def update
-    @tweet = current_user.tweets.find(params[:id])
     if @tweet.update(tweet_params)
       redirect_to root_path
     else
@@ -36,14 +34,22 @@ class TweetsController < ApplicationController
   end
   
   def destroy
-    @tweet = current_user.tweets.find(params[:id])
     @tweet.destroy
     redirect_to root_path
   end
 
 
   private
+
   def tweet_params
     params.require(:tweet).permit(:energy_id,:food_id,:toilet_id,:poop_status_id,:exercise_id,:sleep_id,:different_id,:start_time).merge(user_id: current_user.id)
   end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+    if @tweet.user_id != current_user.id
+      redirect_to action: :index
+    end
+  end
+
 end
